ff41836eeada46b27ad3c3db4f0fc30ef925a2d6
Mahran_AD <mhranabwdqt971@gmail.com>
2025-03-14 00:26:01 +0300

    editing on config->auth.php initial second guard and its provider

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'student' => [
+            'driver' => 'session',
+            'provider' => 'students',
+        ],
     ],
 
     /*
@@ -65,10 +70,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+         'students' => [
+             'driver' => 'eloquent',
+             'model' => App\Models\Student::class,
+         ],
     ],
 
     /*
